1a8f5fcc7e7dc7fd1e1b1d6e05162620eb735fe2
mateu171 <yakimforgithub@gmail.com>
2026-05-29 14:33:11 +0300

    build: configure Maven installer packaging and external config support

--- src/main/java/org/example/bicyclesharing/Launcher.java
+++ src/main/java/org/example/bicyclesharing/Launcher.java
@@ -1,10 +1,14 @@
 package org.example.bicyclesharing;
 
 import javafx.application.Application;
+import org.example.bicyclesharing.util.AppConfig;
 
 public class Launcher {
 
   public static void main(String[] args) {
+    System.out.println(
+        AppConfig.class.getClassLoader().getResource("config/email.properties")
+    );
   Application.launch(HelloApplication.class, args);
 
   }
